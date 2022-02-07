e27bba68d62a81f608eca2b8c8c5cb24078c4e1e
Chad Bonthuys <chad.bonthuys@imaginera.co.uk>
2022-02-07 15:48:54 +0000

    Token config and server connection implementation

--- SerenityBot/Program.cs
+++ SerenityBot/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace SerenityBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+            
+            var token = config.GetValue<string>("Token");
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
